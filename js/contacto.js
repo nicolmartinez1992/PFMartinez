1485e37c13267c1f717822f319c608ae7292d97a
valencha <nicolmartinez1992@hotmail.com>
2023-05-17 13:59:19 -0300

    JS 1359

--- js/contacto.js
+++ js/contacto.js
@@ -1,83 +1,62 @@
 // // Formulario
 
 let nombre = document.getElementById("name");
-nombre.onchange = () => {
-    localStorage.setItem("nombreSinEnviar", nombre.value)
-}
-
-let getNombre = localStorage.getItem("nombreSinEnviar")
-nombre.value = getNombre
-
 let email = document.getElementById("email");
-email.onchange = () => {
-    localStorage.setItem("emailSinEnviar", email.value)
-}
-
-let getEmail = localStorage.getItem("emailSinEnviar")
-email.value = getEmail
-console.log(getEmail)
-
 let mensaje = document.getElementById("message");
-mensaje.onchange = () => {
+
+if ((nombre.value === "") || (email.value === "") || (mensaje.value === "")) {
+    nombre.addEventListener("input", () => {
+        localStorage.setItem("nombreSinEnviar", nombre.value)
+    })
+
+    let getNombre = localStorage.getItem("nombreSinEnviar")
+    nombre.value = getNombre
+
+    email.addEventListener("input", () => {
+        localStorage.setItem("emailSinEnviar", email.value)
+    })
+
+    let getEmail = localStorage.getItem("emailSinEnviar")
+    email.value = getEmail
+
+    mensaje.addEventListener("input", () => {
+        localStorage.setItem("mensajeSinEnviar", mensaje.value)
+    })
+
+    let getMensaje = localStorage.getItem("mensajeSinEnviar")
+    mensaje.value = getMensaje
+} else {
+    localStorage.setItem("nombreSinEnviar", nombre.value)
+    localStorage.setItem("emailSinEnviar", email.value)
     localStorage.setItem("mensajeSinEnviar", mensaje.value)
 }
 
-let getMensaje = localStorage.getItem("mensajeSinEnviar")
-mensaje.value = getMensaje
-console.log(getMensaje)
 
 let formulario = document.getElementById("form");
 formulario.addEventListener("submit", validarFormulario);
-let reset = 0;
-let usuario = 0;
+const error = document.createElement("p")
+error.setAttribute("id", "errorMessage")
 
 function validarFormulario(e) {
     e.preventDefault()
     let nombre = document.getElementById("name").value;
     let email = document.getElementById("email").value;
     let mensaje = document.getElementById("message").value;
-    usuario = { nombre, email, mensaje }
+
+    const enviarComponente = document.getElementById("enviar")
+    enviarComponente.append(error)
 
     if ((nombre === "") || (email === "") || (mensaje === "")) {
-    let componenteError = document.getElementById("error")
-      if (!componenteError) {
-        const error = document.createElement("p")
-        error.innerHTML = "<p id='error'>Dejo algun campo vacío</p>"
-        const enviarComponente = document.getElementById('enviar')
-        enviarComponente.append(error)
-      } else {
-        return null
-      }
-} else {
-    console.log(usuario)
-    localStorage.setItem("usuario", JSON.stringify(usuario))
-    reset = document.getElementById("form").reset();
+        error.innerText = "Dejo algun campo vacío"
+    } else {
+        const usuario = {
+            nombre,
+            email,
+            mensaje
+        }
+        localStorage.setItem("usuario", JSON.stringify(usuario))
+        reset = document.getElementById("form").reset();
+        const error = document.getElementById("errorMessage")
+        error.innerText = ""
+    }
 }
-}
-
-
-    // console.log(componenteError)
-    // if (!componenteError) {
-    //     const error = document.createElement("p")
-    //     error.innerHTML = "<p id='error'>Dejo algun campo vacío</p>"
-    //     const enviarComponente = document.getElementById('enviar')
-    //     enviarComponente.append(error)
-    // }
-
-
-
-
-
-
-
-
-
-
-// let nombreSinEnviar = document.getElementById("name");
-
-// nombreSinEnviar.addEventListener("input", () => {
-//     localStorage.setItem("nombreSinEnviar", nombreSinEnviar.value)
-// })
-
-// let get = localStorage.getItem("nombreSinEnviar")
-// nombreSinEnviar.value = get
